d8797fe8ea0350565e25d72aa54c75b4a9a5f34a
Григорий <grehunter01@gmail.com>
2025-09-20 21:37:39 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
-from fastapi.testclient import TestClient
-from app.main import app
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app.main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
