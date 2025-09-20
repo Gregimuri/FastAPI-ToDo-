688c5ee9d75c491ff6fd4a946df14ebaf5c20ed4
Григорий <grehunter01@gmail.com>
2025-09-20 21:14:58 +0300

    Create crud.py

---
+++ app/crud.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_todos(db: Session):
+    return db.query(models.Todo).all()
+
+def get_todo_by_id(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+def create_todo(db: Session, todo: schemas.TodoCreate):
+    db_todo = models.Todo(**todo.dict())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
