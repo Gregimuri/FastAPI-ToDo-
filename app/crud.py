a34e1fa2f813e13d8c75bbdd39c9568d2e20715f
Григорий <grehunter01@gmail.com>
2025-09-20 22:15:24 +0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -5,7 +5,7 @@ def get_todos(db: Session):
     return db.query(models.Todo).all()
 
 def create_todo(db: Session, todo: schemas.TodoCreate):
-    db_todo = models.Todo(**todo.model_dump())
+    db_todo = models.Todo(**todo.model_dump())  # заменили dict() на model_dump()
     db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
