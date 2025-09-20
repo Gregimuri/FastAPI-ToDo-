34b388a74d7b255e3ba47b9502362b6ae8332f95
Григорий <grehunter01@gmail.com>
2025-09-20 22:10:10 +0300

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -1,11 +1,11 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-def get_todos(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Todo).offset(skip).limit(limit).all()
+def get_todos(db: Session):
+    return db.query(models.Todo).all()
 
 def create_todo(db: Session, todo: schemas.TodoCreate):
-    db_todo = models.Todo(**todo.model_dump())  # заменили .dict() → .model_dump()
+    db_todo = models.Todo(**todo.model_dump())
     db.add(db_todo)
     db.commit()
     db.refresh(db_todo)
